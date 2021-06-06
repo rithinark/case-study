457c7b648f9263a5ee62dccb7e5844341d85dcbb
Rithin <rithin@pop-os.localdomain>
2021-06-06 19:09:22 +0530

    final

--- dice roll/static/index.js
+++ dice roll/static/index.js
@@ -15,8 +15,26 @@ let pl2Random = Math.round(Math.random()*6)
 document.getElementById("pl1-dice").setAttribute('src',List[pl1Random]);
 document.getElementById("pl2-dice").setAttribute('src',List[pl2Random]);
  
- if(pl1Random > pl2random){
-    document.querySelector("h2").textContent;
+
+setTimeout(()=>{
+ if(pl1Random > pl2Random){
+    document.querySelector("#winner").innerHTML= "player 1 wins"; 
+
  }
+ 
+ else if (pl1Random === pl2Random){
+   document.querySelector("#winner").innerHTML= "draw the match";
+ }    
+ 
  else{
-    // ama epadi enaku katum terminalla shared terminals nu sidela iruka? athula python click pani parunga
+   document.querySelector("#winner").innerHTML= "player 2 wins"; 
+  
+}
+},3000);
+ 
+// rpaniten random number uh love calculator bmi calculator la paniyachey
+//fb like button antha mathri click pana like color ila antha mathri eriyanum again click pana ponum
+//vera ena iruku unga molaila avalo use panni solungalen perusa soliruva :Dunga moola sze vena en moola size pothum en moolayum sirusu tha hahaha seri ok inaiku eppadiyo mudistom nxt en clg ppt clg class la ipovum nadakutha  ama ama njy haha oru quiz game eppadi oru 
+//idea?  ithum perusa tha irukum polaama ama  last anupunathu? athu ena per per la thrla  ila ena athula pandrathu.. range vechu panina amount yerumaa ama ithayum panitingala ena ila ila range use panirken aana athua number add pandrathu ithu apo doubts iruku apo ithayae panirunga hehe done ye okiee  okj done ye polama 
+//js la ena topic ini ungaluku?  async await vanthrucha?
+//https://www.frontendmentor.io/challenges/interactive-pricing-component-t0m8PIyY8 itha panunga antha slide pana athula irukra value melayo kilayo sidleayo katanum avlotha
